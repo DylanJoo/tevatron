b3c8f1e28e273a8aea009d12b589b5165fea277d
DylanJoo <dylanjootw@gmail.com>
2025-12-03 18:09:29 +0100

    update encoder param

--- src/tevatron/retriever/driver/encode.py
+++ src/tevatron/retriever/driver/encode.py
@@ -44,7 +44,8 @@ def main():
         level=logging.INFO if training_args.local_rank in [-1, 0] else logging.WARN,
     )
 
-    print("The model arguemnts:", model_args)
+    print("# The model arguemnts:", model_args)
+    print("# The data arguemnts:", data_args)
     tokenizer = AutoTokenizer.from_pretrained(
         model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path,
         cache_dir=model_args.cache_dir
