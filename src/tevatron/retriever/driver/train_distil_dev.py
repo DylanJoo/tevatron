93e80ca4b09ed60a0a1c137f288db37e0c4b2e96
Dylan@lumi <dylanjootw@gmail.com>
2025-12-08 16:28:51 +0200

    add validator and add dev training function

---
+++ src/tevatron/retriever/driver/train_distil_dev.py
@@ -0,0 +1,134 @@
+import logging
+import os
+import sys
+import torch
+from transformers import AutoTokenizer
+from transformers import (
+    HfArgumentParser,
+    set_seed,
+)
+from transformers.trainer_utils import get_last_checkpoint
+
+from tevatron.retriever.arguments import ModelArguments, DataArguments, \
+    TevatronTrainingArguments as TrainingArguments
+from tevatron.retriever.dataset import DistilTrainDataset
+from tevatron.retriever.dataset_dev import QrelDataset
+from tevatron.retriever.collator import DistilTrainCollator, EncodeCollator
+from tevatron.retriever.modeling import DenseModel
+from tevatron.retriever.trainer import DistilTevatronTrainer as DistilTrainer
+
+from tevatron.retriever.callback.nanobeir_eval import Validator
+logger = logging.getLogger(__name__)
+
+
+def main():
+    parser = HfArgumentParser((ModelArguments, DataArguments, TrainingArguments))
+
+    if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
+        model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
+    else:
+        model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+        model_args: ModelArguments
+        data_args: DataArguments
+        training_args: TrainingArguments
+
+    if (
+            os.path.exists(training_args.output_dir)
+            and os.listdir(training_args.output_dir)
+            and training_args.do_train
+            and not training_args.overwrite_output_dir
+    ):
+        raise ValueError(
+            f"Output directory ({training_args.output_dir}) already exists and is not empty. Use --overwrite_output_dir to overcome."
+        )
+
+    # Setup logging
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO if training_args.local_rank in [-1, 0] else logging.WARN,
+    )
+    logger.warning(
+        "Process rank: %s, device: %s, n_gpu: %s, distributed training: %s, 16-bits training: %s",
+        training_args.local_rank,
+        training_args.device,
+        training_args.n_gpu,
+        bool(training_args.local_rank != -1),
+        training_args.fp16,
+    )
+    logger.info("Training/evaluation parameters %s", training_args)
+    logger.info("MODEL parameters %s", model_args)
+
+    set_seed(training_args.seed)
+
+    # Load tokenizer and model
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path,
+        cache_dir=model_args.cache_dir,
+    )
+
+    if tokenizer.pad_token_id is None:
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+
+    if data_args.padding_side == 'right':
+        tokenizer.padding_side = 'right'
+    else:
+        tokenizer.padding_side = 'left'
+
+    if training_args.bf16:
+        torch_dtype = torch.bfloat16
+    elif training_args.fp16:
+        torch_dtype = torch.float16
+    else:
+        torch_dtype = torch.float32
+    
+    model = DenseModel.build(
+        model_args,
+        training_args,
+        cache_dir=model_args.cache_dir,
+        torch_dtype=torch_dtype,
+        attn_implementation=model_args.attn_implementation,
+    )
+
+    # data_args.dataset_name = data_args.dataset_name[0]
+    # data_args.dataset_split = data_args.dataset_split[0]
+    # data_args.corpus_name = data_args.corpus_name[0]
+    # Train dataset and collator
+    print(data_args)
+    train_dataset = DistilTrainDataset(data_args)
+
+    collator = DistilTrainCollator(data_args, tokenizer, torch_dtype)
+    if training_args.do_eval:
+        eval_dataset = QrelDataset(data_args, corpus_name=data_args.eval_corpus_name, is_distil=True)
+    else:
+        eval_dataset = None
+
+    trainer_cls = DistilTrainer
+    trainer = trainer_cls(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        data_collator=collator
+    )
+
+    encode_collator = EncodeCollator(data_args=data_args, tokenizer=tokenizer)
+    # TODO: integrate the validator better
+    trainer.set_validator(Validator(encode_collator, 512))
+
+    train_dataset.set_trainer(trainer)
+    if training_args.do_eval:
+        eval_dataset.set_trainer(trainer)
+    
+    last_checkpoint = None
+    if os.path.isdir(training_args.output_dir):
+        last_checkpoint = get_last_checkpoint(training_args.output_dir)
+
+    trainer.train(resume_from_checkpoint=(last_checkpoint is not None))
+    trainer.save_model()
+    if trainer.is_world_process_zero():
+        tokenizer.save_pretrained(training_args.output_dir)
+
+
+if __name__ == "__main__":
+    main()
